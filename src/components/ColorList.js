22a56421af7eec8187bb50858fb6645ce088b503
Hammam AbuShehadeh <hammam.y.asmar@gmail.com>
2023-11-10 23:23:34 +0300

    Completed assignment

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,5 +1,59 @@
 import React from "react";
 
+// ColorItem component
+function ColorItem(props) {
+  return <li style={{ color: props.color }}>{props.color}</li>;
+}
+
+// ColorList component
+function ColorList() {
+  const colors = [
+    "firebrick",
+    "rebeccapurple",
+    "salmon",
+    "darkslategray",
+    "hotpink",
+  ];
+
+  const colorElements = colors.map((color) => {
+    return <ColorItem key={color} color={color} />;
+  });
+  return (
+    <div>
+      <h1>Top 5 CSS Colors</h1>
+      <ol>
+        {/* display the array of <li> elements here! */}
+        {colorElements}
+      </ol>
+    </div>
+  );
+}
+/*
+function ColorList() {
+  const colors = [
+    "firebrick",
+    "rebeccapurple",
+    "salmon",
+    "darkslategray",
+    "hotpink",
+  ];
+
+  const colorElements = colors.map((color) => {
+    return <li key={color} style={{ color: color }}>{color}</li>;
+  });
+  return (
+    <div>
+      <h1>Top 5 CSS Colors</h1>
+      <ol>
+        {/* display the array of <li> elements here! *//*}
+        {colorElements}
+      </ol>
+    </div>
+  );
+}
+*/
+
+/*
 function ColorList() {
   const colors = [
     "firebrick",
@@ -22,5 +76,5 @@ function ColorList() {
     </div>
   );
 }
-
+*/
 export default ColorList;
